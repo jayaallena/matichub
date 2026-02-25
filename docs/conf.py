6319d0bf13c725bb58fb31c41dc451fd190826ce
jayaallena <jayaallena@outlook.com>
2026-02-25 15:36:32 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+project = 'duckduckgo-login'
+author = 'duckduckgo-login'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
